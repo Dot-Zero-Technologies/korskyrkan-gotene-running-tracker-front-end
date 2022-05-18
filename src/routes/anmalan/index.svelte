9027c19b5fc99f8a23c065d430e50e02e0e98ac2
Esaias Tilly <esaias@tillys.se>
2022-05-18 19:35:30 +0200

    Centered main

--- src/routes/anmalan/index.svelte
+++ src/routes/anmalan/index.svelte
@@ -3,7 +3,19 @@
 
 <!-- Header -->
 <!-- Main -->
-<main />
+<div class="container">
+	<main>This is a test</main>
+</div>
 
 <style>
+	.container {
+		display: flex;
+		justify-content: center;
+		width: 100%;
+		height: auto;
+	}
+
+	main {
+		padding: 1rem;
+	}
 </style>
