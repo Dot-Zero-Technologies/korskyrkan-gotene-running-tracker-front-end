3d0976a48943dbfe4244155b7c4270645aaa14cf
Esaias Tilly <esaias@tillys.se>
2022-05-19 21:36:51 +0200

    Added nav to header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
 	import Logo from './Logo.svelte'
+	import Nav from './Nav.svelte'
+	import NavItem from './NavItem.svelte'
 </script>
 
 <div class="container">
 	<header>
 		<div class="logo"><Logo /></div>
+		<Nav>
+			<NavItem title="Hem" href="/" active={true} />
+			<NavItem title="På Gång" href="/" active={false} />
+		</Nav>
 	</header>
 </div>
 
---
+++ src/components/Nav.svelte
@@ -0,0 +1,12 @@
+<nav class="container">
+	<slot />
+</nav>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: left;
+		width: 100%;
+		height: 48px;
+	}
+</style>
