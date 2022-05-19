814afe60a550b71f21437c37a4f9723dec9568e1
Esaias Tilly <esaias@tillys.se>
2022-05-19 21:21:14 +0200

    Added basic header with logo

---
+++ src/components/Header.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Logo from './Logo.svelte'
+</script>
+
+<div class="container">
+	<header>
+		<div class="logo"><Logo /></div>
+	</header>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: center;
+		width: 100%;
+		height: auto;
+	}
+
+	header {
+		display: block;
+		padding: 1rem;
+		width: 100%;
+		max-width: 80rem;
+	}
+</style>
---
+++ src/components/Logo.svelte
@@ -0,0 +1,4 @@
+<img
+	src="https://korskyrkangotene.se/wp-content/uploads/2020/03/Korskyrkan-logo-Color-272x90-1.png"
+	alt="Korskyrkan Götene"
+/>
