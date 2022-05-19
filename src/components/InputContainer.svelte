db3f70d991e05317a859e37db9420c46473037ef
Esaias Tilly <esaias@tillys.se>
2022-05-19 20:30:53 +0200

    Added input label

---
+++ src/components/InputContainer.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Input from './Input.svelte';
+	import InputLabel from './InputLabel.svelte';
+	export let label: string;
+	export let id: string;
+	export let value: string;
+
+	interface $$Props extends Partial<HTMLInputElement> {
+		label: string;
+		id: string;
+		value: string;
+	}
+</script>
+
+<div class="input-container">
+	<InputLabel for_id={id}>{label}</InputLabel>
+	<Input bind:value {id} {...$$restProps} />
+</div>
+
+<style>
+	.input-container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		margin-top: 0.5rem;
+	}
+</style>
