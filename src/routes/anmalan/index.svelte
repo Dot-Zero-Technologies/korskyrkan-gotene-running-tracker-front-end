0e226a1088cb34627a0843a78dfd24f6f9d6b574
Esaias Tilly <esaias@tillys.se>
2022-05-20 11:40:25 +0200

    Bind input values

--- src/routes/anmalan/index.svelte
+++ src/routes/anmalan/index.svelte
@@ -3,6 +3,16 @@
 	import InputContainer from '../../components/InputContainer.svelte'
 	import Button from '../../components/Button.svelte'
 	import Header from '../../components/Header.svelte'
+
+	let runnerName: string = ''
+	let runnerPhone: string = ''
+	let sponsorName: string = ''
+	let sponsorPhone: string = ''
+	let sponsorAmount: number = 0
+
+	const sendRegistration = () => {
+		console.log(runnerName, runnerPhone, sponsorName, sponsorPhone, sponsorAmount)
+	}
 </script>
 
 <!-- Header -->
@@ -13,23 +23,28 @@
 	<main>
 		<h3>Löpare</h3>
 		<Indent>
-			<InputContainer type="text" label="Namn" id="name" value="" />
-			<InputContainer type="tel" label="Telefonnummer" id="phone" value="" />
+			<InputContainer type="text" label="Namn" id="name" bind:value={runnerName} />
+			<InputContainer type="tel" label="Telefonnummer" id="phone" bind:value={runnerPhone} />
 		</Indent>
 
 		<h3>Sponsor</h3>
 		<Indent>
-			<InputContainer type="text" label="Namn" id="sponsorName" value="" />
-			<InputContainer type="tel" label="Telefonnummer" id="sponsorPhone" value="" />
+			<InputContainer type="text" label="Namn" id="sponsorName" bind:value={sponsorName} />
+			<InputContainer
+				type="tel"
+				label="Telefonnummer"
+				id="sponsorPhone"
+				bind:value={sponsorPhone}
+			/>
 			<InputContainer
 				type="number"
 				label="Sponsrad summa (kr)"
 				id="sponsorAmount"
-				value=""
+				bind:value={sponsorAmount}
 				info="Summa (i kr) som sponsras per varv som löparen springer."
 			/>
 
-			<Button text="Skicka anmälan" />
+			<Button text="Skicka anmälan" on:click={sendRegistration} />
 		</Indent>
 	</main>
 </div>
